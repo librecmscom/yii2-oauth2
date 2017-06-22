c762b63b91323d0cca9374e74ed913f1465c2909
xuchao <85825770@qq.com>
2017-06-22 13:40:01 +0800

    update

--- filters/auth/TokenAuth.php
+++ filters/auth/TokenAuth.php
@@ -5,10 +5,11 @@
  * @license https://github.com/borodulin/yii2-oauth2-server/blob/master/LICENSE
  */
 
-namespace yuncms\oauth2;
+namespace yuncms\oauth2\filters\auth;
 
 use Yii;
 use yii\filters\auth\AuthMethod;
+use yuncms\oauth2\Exception;
 use yuncms\oauth2\models\AccessToken;
 
 /**
@@ -89,7 +90,7 @@ class TokenAuth extends AuthMethod
     protected function getAccessToken()
     {
         if (is_null($this->_accessToken)) {
-            $request = \Yii::$app->request;
+            $request = Yii::$app->request;
 
             $authHeader = $request->getHeaders()->get('Authorization');
 
