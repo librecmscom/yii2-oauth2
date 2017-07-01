fffe690341c7f07fc891630f7e59ebccd8cf3385
xuchao <85825770@qq.com>
2017-07-01 10:02:52 +0800

    update

--- views/client/_form.php
+++ views/client/_form.php
@@ -18,13 +18,14 @@ use yii\bootstrap\ActiveForm;
 <?= $form->field($model, 'domain') ?>
 <?= $form->field($model, 'provider') ?>
 <?= $form->field($model, 'icp') ?>
-<?= $form->field($model, 'grant_type')->dropDownList(['authorization_code' => 'Authorization Code', 'password' => 'Password'],[
+<?= $form->field($model, 'grant_type')->dropDownList(['authorization_code' => 'Authorization Code', 'password' => 'Password'], [
     'prompt' => Yii::t('oauth2', 'All Type')
 ]); ?>
 <?= $form->field($model, 'redirect_uri'); ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('oauth2', 'Create') : Yii::t('oauth2', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <div class="col-sm-offset-3 col-sm-9">
+            <?= Html::submitButton($model->isNewRecord ? Yii::t('oauth2', 'Create') : Yii::t('oauth2', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
     </div>
-
 <?php ActiveForm::end(); ?>
